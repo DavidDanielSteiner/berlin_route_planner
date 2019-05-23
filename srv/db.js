6baa7f7f60496280fbff461d4295891b01cd60c4
artur.gevert <gewert92@gmail.com>
2019-05-23 12:11:58 +0200

    merged with Artur's project

--- srv/db.js
+++ srv/db.js
@@ -22,55 +22,7 @@ module.exports = {
         });
     },
 
-
-    /**
-    Aufgabe 2.1 Insert Test Data (Key Value) into Hana Database
-     */
-    insertIntoHdbTestData: function (hdb, sql, records, infoHandler) {
-        connection.connect(hdb, (err) => {
-            if (err) {
-                return console.error("Connection error", err);
-            }
-            for (const c of records) {
-                const params = [c.key, c.value];
-                var stmt = connection.prepare(sql);
-                stmt.exec(params, (err) => {
-                    if (err) {
-                        return console.error('SQL execute error:', err);
-                    }
-                    infoHandler(`Query '${sql}' successful. Values: '${params}'`);
-                });
-            }
-        });
-
-    },
-
-    /**
-     Aufgabe 2.2 Insert Station Data into Hana Database
-     uuid,name,brand,street,house_number,post_code,city,latitude,longitude
-   */
-    insertIntoHdbStations: function (hdb, sql, records, infoHandler) {
-        connection.connect(hdb, (err) => {
-            if (err) {
-                return console.error("Connection error", err);
-            }
-            for (const c of records) {
-                const params = [c.uuid, c.name, c.brand, c.street, c.house_number, c.post_code, c.city, c.latitude, c.longitude];
-                var stmt = connection.prepare(sql);
-                stmt.exec(params, (err) => {
-                    if (err) {
-                        return console.error('SQL execute error:', err);
-                    }
-                    //infoHandler(`Query '${sql}' successful.`);
-                });
-            }infoHandler(`Import finished.`);
-        });
-    },
-
-    /**
-     Aufgabe 2.3 get all Stations in 'distance' from 'lat', 'lng'
-    */
-    readFromHdbStations: function (hdb, sql, params, handleRows, infoHandler) {
+    writeInToHDB: function (hdb, sql, params, handleRows, infoHandler) {
         connection.connect(hdb, (err) => {
             if (err) {
                 return console.error("Connection error", err);
@@ -79,18 +31,25 @@ module.exports = {
             var stmt = connection.prepare(sql);
             stmt.exec(params, (err, rows) => {
                 connection.disconnect();
+                //connection.query(sql,)
+                /** connection.query(sql, [params], function (err, result) {  
+                    if (err) throw err;  
+                    console.log("Number of records inserted: " + result.affectedRows);  
+                    });  */
 
                 if (err) {
-                    return console.error('SQL execute error:', err);
-                }
+                        return console.error('SQL execute error:', err);
+                    }
                 handleRows(rows);
-                infoHandler(`Query '${sql}' returned ${rows.length} stations`);
+                infoHandler(`Query '${sql}' saved ${rows.length} items`);
+                /** connection.end(function(err){
+                    if (err) throw err;
+                    connection.disconnect();
+
+                });*/
+    
             });
-
         });
-    }
-
+    },
 
 };
-
-
--- srv/srv.js
+++ srv/srv.js
@@ -4,10 +4,7 @@ const port = 3000
 
 const config = require('./config');
 const db = require('./db');
-
-//Added packages
-const http = require('https');
-const parse = require('csv-parse/lib/sync')
+const request = require('request');
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -19,13 +16,12 @@ app.get('/db', (req, res, next) => {
     const sql = `SELECT * FROM adbkt.reg where id=?`;
     var dbid = req.query.dbid;
     console.log(`dbid: ${dbid}`);
-
     const params = [dbid];
     db.readFromHdb(
         config.hdb,
         sql,
         params,
-        rows => res.type('application/json').send(rows),
+        rows => res.type('application/json').send(rows), 
         info => console.log(info)
     );
 });
@@ -34,90 +30,66 @@ app.get('/mult', (req, res, next) => {
     res.send(`${req.query.num1 * req.query.num2}`);
 });
 
-
-/**
-Aufgabe 2.1 Get TestData from Web CSV, Insert TestData into Hdb
- */
-app.get('/importTestData', (req, res, next) => {
-   var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-   const Http = new XMLHttpRequest();
-   const url = 'https://wiki.htw-berlin.de/confluence/download/attachments/31623434/test.txt?version=1&modificationDate=1552985182810&api=v2';
-   Http.open("GET", url);
-   Http.send();
-   Http.onreadystatechange=function(){
-       if(this.readyState==4 && this.status==200) {
-           const records = parse(Http.responseText, {
-               columns: true,
-               trim: true,
-               skip_empty_lines: true
-           })
-           //console.log(records);
-           const sql = `INSERT INTO \"U556414\".\"IMPORT\" VALUES (?, ?)`;
-           db.insertIntoHdbTestData(
-               config.hdb,
-               sql,
-               records,
-               info => console.log(info)
-           );
-       }
-   }
-});
-
-/**
-Aufgabe 2.2 Get StationData from Web, Parse Data from CSV format, Insert Data into Database
- */
-app.get('/importStations', (req, res, next) => {
-    var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-    const Http = new XMLHttpRequest();
-    //https://tankerkoenig@dev.azure.com/tankerkoenig/tankerkoenig-data/_git/tankerkoenig-data
+app.get('/import', (req, res, next) => {
+    const parse = require('csv-parse');
     const url = 'https://dev.azure.com/tankerkoenig/362e70d1-bafa-4cf7-a346-1f3613304973/_apis/git/repositories/0d6e7286-91e4-402c-af56-fa75be1f223d/Items?path=%2Fstations%2Fstations.csv&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&download=true&resolveLfs=true&%24format=octetStream&api-version=5.0-preview.1';
-    Http.open("GET", url);
-    Http.send();
-    Http.onreadystatechange=function(){
-        if(this.readyState==4 && this.status==200) {
-            const records = parse(Http.responseText, {
-                columns: true,
-                trim: true,
-                skip_empty_lines: true
-            })
-            //console.log(records);
-            const sql = `INSERT INTO "U556414"."STATIONS2" VALUES (?,?,?,?,?,?,?,?,?)`;
 
-            db.insertIntoHdbStations(
-                config.hdb,
-                sql,
-                records,
-                info => console.log(info)
-            );
-        }
-    }
+    var sSQLStatement = "INSERT INTO TANKSTELLENDATEN VALUES (?,?,?,?,?,?,?,?,?,null)";
+    const parser = parse({
+        delimiter: ',',
+        
+        }, function(err, records){
+          records.shift();
+          console.log(records)
+          res.send(records);
+            db.writeInToHDB(
+            config.hdb,
+            sSQLStatement,
+            records,
+            rows => console.log (rows.length),
+            info => console.log(info)
+        );
+
+        if (err) {
+            return console.error('Import failed:', err);
+        }} 
+      );
+    //get data from url and parse into json -> write into hdb
+    request(url).pipe(parser); 
 });
 
+app.get('/outbound', (req, res, next) => {
+    /**
+     * Before: to be done in SQL Editor
+     * 'UPDATE Tankstellendaten SET Shape_4326 = NEW ST_Point('Point ('|| latitude ||' '|| longitude||') ',4326)'
+     */
 
-/**
-Aufgabe 2.3
- */
-app.get('/stations', (req, res, next) => {
-    var lat = req.query.lat;
-    var lng = req.query.lng;
-    var distance = req.query.distance;
-    console.log(lat)
-    console.log(lng)
-    console.log(distance)
+    //Get data from form
+    var sGetLon = req.query.lng;
+    var sGetLat = req.query.lat;
+    var sGetDistance = req.query.distance;
+    //Define SQL Statement
+    const sSQLStatement = "SELECT * FROM Tankstellendaten where (shape_4326.st_distance(NEW ST_Point('Point ('|| ? ||' '|| ?||')',4326),'kilometer'))<?;";
 
-    const sql = `SELECT name_station, brand, street, house_number, post_code, city, NEW ST_POINT(LATITUDE, LONGITUDE).ST_SRID(4326).ST_Distance( NEW ST_Point('POINT('||?||' '||?||')', 4326), 'kilometer') AS distance_km FROM "U556414"."STATIONS2" WHERE NEW ST_POINT(LATITUDE, LONGITUDE).ST_SRID(4326).ST_Distance( NEW ST_Point('POINT('||?||' '||?||')', 4326), 'kilometer') <= ?`;
-    const params = [lat, lng, lat, lng,distance];
+    
+    
+    var params_daten =[];
+    
+    //Fill in required data for sql statement
+    params_daten.push(sGetLat);
+    params_daten.push(sGetLon);
+    params_daten.push(sGetDistance);
+    
+    console.log(params_daten);
 
-    db.readFromHdbStations(
+    // run sql statement and send rows to view
+    db.readFromHdb(
         config.hdb,
-        sql,
-        params,
+        sSQLStatement,
+        params_daten,
         rows => res.type('application/json').send(rows),
-        info => console.log(info)
-    );
+        info => console.log(info));
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-
-
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
