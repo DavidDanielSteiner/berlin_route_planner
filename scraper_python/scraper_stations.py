1d82c10cd5f75c8db173a2d725f57cc7a9e18ea9
DavidDanielSteiner <38961224+DavidDanielSteiner@users.noreply.github.com>
2019-05-23 23:32:42 +0200

    upload scraper and station_data.csv

---
+++ scraper_python/scraper_stations.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 23 15:31:52 2019
+
+@author: David
+"""
+
+
+import mysql.connector
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import re
+
+url = 'https://de.wikipedia.org/wiki/Liste_der_Berliner_U-Bahnh%C3%B6fe'
+resp = requests.get(url)
+soup = BeautifulSoup(resp.text, 'lxml')
+
+stationTable = soup.findAll('table',{"class":"wikitable sortable"})[1]
+stationTable_rows = stationTable.find_all('tr')   
+
+stations_data = []
+
+stationTable_rows = stationTable_rows[1:]
+
+for num, element in enumerate(stationTable_rows, start =0): 
+    
+    station_data = []    
+    td = stationTable_rows[num].find_all('td')
+            
+    for num, element in enumerate(td[0].find_all('a'), start=0):
+        if num == 1:
+            geodata = element.getText()
+            geodata_split = geodata.split(',')
+            geodata_split[0]       
+            latitude_dms = re.findall("[-+]?[.]?[\d]+(?:,\d\d\d)*[\.]?\d*(?:[eE][-+]?\d+)?", geodata_split[0])
+            latitude = convert_dms_to_decimal(latitude_dms)
+            longitude_dms = re.findall("[-+]?[.]?[\d]+(?:,\d\d\d)*[\.]?\d*(?:[eE][-+]?\d+)?", geodata_split[1])
+            longitude = convert_dms_to_decimal(longitude_dms) 
+            station_data.append(latitude)
+            station_data.append(longitude)
+        else:
+            station_data.append(element.getText())
+                
+    station_data.append(td[1]['data-sort-value'])
+    station_data.append(td[4].getText())
+    station_data.append('https://de.wikipedia.org' + td[0].find('a')['href']) 
+    stations_data.append(station_data)
+
+df=pd.DataFrame(stations_data ,columns=['station_name','latitude','longitude','line_name','district_name', 'station_link'])  
+df2= df.dropna()
+
+df.to_csv (r'C:\Users\David\Desktop\all_stations.csv', index = None, header=True) 
+
+def convert_dms_to_decimal(dms):
+    decimal = (((int(dms[2]) /60) + int(dms[1]))/60 + int(dms[0]))
+    return decimal
