88af3e0501e827b74551ac6160199705d728d9ec
David Steiner <38961224+DavidDanielSteiner@users.noreply.github.com>
2019-05-24 17:07:51 +0200

    add scaper_subwayLines

--- scraper_python/scraper_subwayLines.py
+++ scraper_python/scraper_subwayLines.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu May 23 23:28:30 2019
+Created on Fri May 24 16:04:13 2019
 
+@author: U722571
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 23 23:28:30 2019
 @author: David
 """
 
@@ -9,83 +15,195 @@ Created on Thu May 23 23:28:30 2019
 # =============================================================================
 # 
 # =============================================================================
-url = 'https://de.wikipedia.org/wiki/Liste_der_Berliner_U-Bahnh%C3%B6fe'
+
+
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import re
+
+
+
+# =============================================================================
+#    RegEx 
+# =============================================================================
+    
+import re
+s = 'I love book()'
+result = re.search(r'\(\)',s)
+print result.group()
+s1 = 'I love book(s)'
+result2 = re.sub(r'[\(\)]','',s1)
+print result2
+
+
+patterns = [ '\(', '\)']
+text = 'Does this text() match the pattern?'
+
+for pattern in patterns:
+    print('Looking for "%s" in "%s" ->' % (pattern, text)),
+
+    if re.search(pattern,  text):
+        print('found a match!')
+    else:
+        print('no match')
+
+
+
+patterns = '\('
+text = 'xt'
+
+if re.search(pattern,  text):
+    print('found a match!')
+else:
+    print('no match')
+
+
+stations_data = []
+
+#for i in range(10):
+    
+i = 1
+
+url = 'https://de.wikipedia.org/wiki/U-Bahn-Linie_' + i + '_(Berlin)'
 resp = requests.get(url)
 soup = BeautifulSoup(resp.text, 'lxml')
 
+#get Table with Station Data
+stationTable = soup.findAll('table',{"class":"noviewer"})
 
-
-stationTable = soup.findAll('table',{"class":"wikitable sortable"})[1]
+#loop all Table Rows
 stationTable_rows = stationTable.find_all('tr')   
-
-l = []
-for tr in stationTable_rows:
-    td = tr.find_all('td')
-    td[1].find('a').getText()
-     
-    row = [tr.text for tr in td]
-    l.append(row)
-       
-pd.DataFrame(l, columns=["A", "B", ...])
-
-
-
-txt = "apple#banana#cherry#orange"
-x = txt.split("#", 1)
-
-https://de.wikipedia.org/wiki/U-Bahnhof_Afrikanische_Stra%C3%9Fe
-
-td[0]
-
-for a in td[0].find_all('a')
-    print(a['href'])
+for num, element in enumerate(stationTable_rows, start =1): 
     
-td[0].find('a')['href']
+    station_data = []    
+    #get Tablecells
+    td = stationTable_rows[num].find_all('td')
     
-station_data.append(a.getText())
-    
-    
-#img = td[1].find('img', alt=True)
-#print(img['alt'])
+    if td[2].getText 
+    print(td[2].find('a')['href']) # == Warschauer Straße 
+    tmp = td[2].getText())  # == (WA)
+    #td[2]['a']['href'].getText()
+    station_data.append(i + num)
+    station_data.append('U' + i)
+    stations_data.append(station_data)
 
-      
-      
-find('data-sort-value')
+df=pd.DataFrame(stations_data ,columns=['station_name','station_id','line_name'])  
+    
+df.to_csv (r'C:\Users\David\Desktop\subway_lines.csv', index = None, header=True) 
+    
 
-soup.select('span#volume')[0].text
-    
-    
-    print("Found the URL:", a['href'])
-    
-    
-x.find_all('a'):
-    a.getText()
-    
-    
-    print "Found the URL:", a['href']
-    
-    <a href="/wiki/U-Bahnhof_Adenauerplatz" title="U-Bahnhof Adenauerplatz">Adenauerplatz</a>
-    <a class="external text" href="https://tools.wmflabs.org/geohack/geohack.php?pagename=Liste_der_Berliner_U-Bahnh%C3%B6fe&amp;language=de&amp;params=52.499722_N_13.307222_E_region:DE-BE_type:landmark&amp;title=Adenauerplatz">52° 29′ 59″ N, 13° 18′ 26″ O</a>
+# =============================================================================
+# for a in td[0].find_all('a')
+#     print(a['href'])
+#     
+# td[0].find('a')['href']
+# 
+# =============================================================================
 
+# =============================================================================
+# HTML CODE
+# =============================================================================
 
-for tag in td:
-    td[1].find('a').getText()
-    for a in soup.find_all('a', href=True):
-    print "Found the URL:", a['href']
-
-
-td data-sort-value="Adenauerplatz"><span id="A"></span><a href="/wiki/U-Bahnhof_Adenauerplatz" title="U-Bahnhof Adenauerplatz">Adenauerplatz</a> (Ad)<br/><small><span class="plainlinks-print" id="Adenauerplatz" title="Koordinate"><a class="external text" href="https://tools.wmflabs.org/geohack/geohack.php?pagename=Liste_der_Berliner_U-Bahnh%C3%B6fe&amp;language=de&amp;params=52.499722_N_13.307222_E_region:DE-BE_type:landmark&amp;title=Adenauerplatz">52° 29′ 59″ N, 13° 18′ 26″ O</a></span></small></td>
-
-rows[2]            
-for row in rows:
-    cells = row.find_all(['th', 'td'])  
-    print(cells)
+<td class="Vorlage_BS-table" colspan="2" style="padding:12px;"><div class="noprint" style="text-align:right;"><small><a href="/wiki/Wikipedia:Formatvorlage_Bahnstrecke/Legende" title="Wikipedia:Formatvorlage Bahnstrecke/Legende">Legende</a></small></div>
+<table class="noviewer" cellpadding="0" style="border-collapse:collapse; line-height:1.2; white-space:nowrap;">
 
 
-    
-    
-    
-    
-    for cell in cells:
-        # DO SOMETHING
-        if cell.string: print (cell.string)
+
+<tbody><tr>
+<td style="text-align:center; padding:0; border:none; white-space:nowrap;"><div style="position:relative; z-index:2; background-color:#F9F9F9;" class="bs-icon"><a href="/wiki/Datei:BSicon_uKBHFa.svg" class="image" title="&nbsp;&nbsp;&nbsp;"><img alt="&nbsp;&nbsp;&nbsp;" src="//upload.wikimedia.org/wikipedia/commons/thumb/1/1f/BSicon_uKBHFa.svg/20px-BSicon_uKBHFa.svg.png" decoding="async" width="20" height="20" srcset="//upload.wikimedia.org/wikipedia/commons/thumb/1/1f/BSicon_uKBHFa.svg/30px-BSicon_uKBHFa.svg.png 1.5x, //upload.wikimedia.org/wikipedia/commons/thumb/1/1f/BSicon_uKBHFa.svg/40px-BSicon_uKBHFa.svg.png 2x" data-file-width="500" data-file-height="500"></a></div>
+</td>
+
+<td style="text-align:right; vertical-align:middle; padding:0 .5em; font-size:80%; white-space:nowrap; border:none;">8,7
+</td>
+
+<td style="padding:0; border:none;" colspan="2"><a href="/wiki/Bahnhof_Berlin_Warschauer_Stra%C3%9Fe" title="Bahnhof Berlin Warschauer Straße">Warschauer Straße</a> (WA) <small><br><a href="/wiki/S-Bahn_Berlin#S3" title="S-Bahn Berlin#S3"><img alt="S3" src="//upload.wikimedia.org/wikipedia/commons/thumb/2/21/Berlin_S3.svg/26px-Berlin_S3.svg.png" decoding="async" width="26" height="13" srcset="//upload.wikimedia.org/wikipedia/commons/thumb/2/21/Berlin_S3.svg/40px-Berlin_S3.svg.png 1.5x, //upload.wikimedia.org/wikipedia/commons/thumb/2/21/Berlin_S3.svg/52px-Berlin_S3.svg.png 2x" data-file-width="514" data-file-height="257"></a> <a href="/wiki/S-Bahn_Berlin#S5" title="S-Bahn Berlin#S5"><img alt="S5" src="//upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Berlin_S5.svg/26px-Berlin_S5.svg.png" decoding="async" width="26" height="13" srcset="//upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Berlin_S5.svg/40px-Berlin_S5.svg.png 1.5x, //upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Berlin_S5.svg/52px-Berlin_S5.svg.png 2x" data-file-width="514" data-file-height="257"></a> <a href="/wiki/S-Bahn_Berlin#S7" title="S-Bahn Berlin#S7"><img alt="S7" src="//upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Berlin_S7.svg/26px-Berlin_S7.svg.png" decoding="async" width="26" height="13" srcset="//upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Berlin_S7.svg/40px-Berlin_S7.svg.png 1.5x, //upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Berlin_S7.svg/52px-Berlin_S7.svg.png 2x" data-file-width="514" data-file-height="257"></a> <a href="/wiki/S-Bahn_Berlin#S9" title="S-Bahn Berlin#S9"><img alt="S9" src="//upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Berlin_S9.svg/26px-Berlin_S9.svg.png" decoding="async" width="26" height="13" srcset="//upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Berlin_S9.svg/40px-Berlin_S9.svg.png 1.5x, //upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Berlin_S9.svg/52px-Berlin_S9.svg.png 2x" data-file-width="514" data-file-height="257"></a> <a href="/wiki/U-Bahn-Linie_3_(Berlin)" title="U-Bahn-Linie 3 (Berlin)"><img alt="U3" src="//upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Berlin_U3.svg/22px-Berlin_U3.svg.png" decoding="async" width="22" height="13" srcset="//upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Berlin_U3.svg/34px-Berlin_U3.svg.png 1.5x, //upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Berlin_U3.svg/44px-Berlin_U3.svg.png 2x" data-file-width="436" data-file-height="258"></a></small>
+</td></tr>
+
+
+
+<tr>
+<td style="text-align:center; padding:0; border:none; white-space:nowrap;"><div style="position:relative; height:0;"><div style="position:absolute; left:0; right:0; bottom:0; width:100%; z-index:1; background-color:#F9F9F9;" class="bs-icon"><a href="/wiki/Datei:BSicon_uSTR.svg" class="image"><img alt="BSicon uSTR.svg" src="//upload.wikimedia.org/wikipedia/commons/thumb/a/a3/BSicon_uSTR.svg/20px-BSicon_uSTR.svg.png" decoding="async" width="20" height="20" srcset="//upload.wikimedia.org/wikipedia/commons/thumb/a/a3/BSicon_uSTR.svg/30px-BSicon_uSTR.svg.png 1.5x, //upload.wikimedia.org/wikipedia/commons/thumb/a/a3/BSicon_uSTR.svg/40px-BSicon_uSTR.svg.png 2x" data-file-width="500" data-file-height="500"></a></div></div>
+</td>
+<td style="padding:0; border:none;">
+</td>
+<td style="padding:0; border:none; width:100%;">
+</td>
+<td style="padding:0; border:none;">
+</td></tr>
+
+
+<tr>
+<td style="text-align:center; padding:0; border:none; white-space:nowrap;"><div style="position:relative; z-index:2; background-color:#F9F9F9;" class="bs-icon"><a href="/wiki/Datei:BSicon_uABZg%2Bl.svg" class="image" title="&nbsp;&nbsp;&nbsp;"><img alt="&nbsp;&nbsp;&nbsp;" src="//upload.wikimedia.org/wikipedia/commons/thumb/6/6b/BSicon_uABZg%2Bl.svg/20px-BSicon_uABZg%2Bl.svg.png" decoding="async" width="20" height="20" srcset="//upload.wikimedia.org/wikipedia/commons/thumb/6/6b/BSicon_uABZg%2Bl.svg/30px-BSicon_uABZg%2Bl.svg.png 1.5x, //upload.wikimedia.org/wikipedia/commons/thumb/6/6b/BSicon_uABZg%2Bl.svg/40px-BSicon_uABZg%2Bl.svg.png 2x" data-file-width="500" data-file-height="500"></a></div>
+</td>
+<td style="text-align:right; vertical-align:middle; padding:0 .5em; font-size:80%; white-space:nowrap; border:none;">
+</td>
+<td style="padding:0; border:none;" colspan="2"><small>zur Abstellanlage Warschauer Straße</small>
+</td></tr>
+
+
+
+
+<tr>
+<td style="text-align:center; padding:0; border:none; white-space:nowrap;"><div style="position:relative; z-index:2; background-color:#F9F9F9;" class="bs-icon"><a href="/wiki/Datei:BSicon_ueHST.svg" class="image" title="&nbsp;&nbsp;&nbsp;"><img alt="&nbsp;&nbsp;&nbsp;" src="//upload.wikimedia.org/wikipedia/commons/thumb/5/57/BSicon_ueHST.svg/20px-BSicon_ueHST.svg.png" decoding="async" width="20" height="20" srcset="//upload.wikimedia.org/wikipedia/commons/thumb/5/57/BSicon_ueHST.svg/30px-BSicon_ueHST.svg.png 1.5x, //upload.wikimedia.org/wikipedia/commons/thumb/5/57/BSicon_ueHST.svg/40px-BSicon_ueHST.svg.png 2x" data-file-width="500" data-file-height="500"></a></div>
+</td>
+<td style="text-align:right; vertical-align:middle; padding:0 .5em; font-size:80%; white-space:nowrap; border:none;">
+</td>
+<td style="padding:0; border:none;" colspan="2"><small><a href="/wiki/U-Bahnhof_Osthafen" title="U-Bahnhof Osthafen">Osthafen</a> <small>bis 1945</small></small>
+</td></tr>
+
+
+
+
+<tr>
+<td style="text-align:center; padding:0; border:none; white-space:nowrap;"><div style="position:relative; z-index:2; background-color:#F9F9F9;" class="bs-icon"><a href="/wiki/Datei:BSicon_uBHF.svg" class="image" title="&nbsp;&nbsp;&nbsp;"><img alt="&nbsp;&nbsp;&nbsp;" src="//upload.wikimedia.org/wikipedia/commons/thumb/c/c8/BSicon_uBHF.svg/20px-BSicon_uBHF.svg.png" decoding="async" width="20" height="20" srcset="//upload.wikimedia.org/wikipedia/commons/thumb/c/c8/BSicon_uBHF.svg/30px-BSicon_uBHF.svg.png 1.5x, //upload.wikimedia.org/wikipedia/commons/thumb/c/c8/BSicon_uBHF.svg/40px-BSicon_uBHF.svg.png 2x" data-file-width="500" data-file-height="500"></a></div>
+</td>
+<td style="text-align:right; vertical-align:middle; padding:0 .5em; font-size:80%; white-space:nowrap; border:none;">7,9
+</td>
+<td style="padding:0; border:none;" colspan="2"><a href="/wiki/U-Bahnhof_Schlesisches_Tor" title="U-Bahnhof Schlesisches Tor">Schlesisches Tor</a> (S) <small><a href="/wiki/U-Bahn-Linie_3_(Berlin)" title="U-Bahn-Linie 3 (Berlin)"><img alt="U3" src="//upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Berlin_U3.svg/22px-Berlin_U3.svg.png" decoding="async" width="22" height="13" srcset="//upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Berlin_U3.svg/34px-Berlin_U3.svg.png 1.5x, //upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Berlin_U3.svg/44px-Berlin_U3.svg.png 2x" data-file-width="436" data-file-height="258"></a></small>
+</td></tr>
+
+
+
+
+<tr>
+<td style="text-align:center; padding:0; border:none; white-space:nowrap;"><div style="position:relative; z-index:2; background-color:#F9F9F9;" class="bs-icon"><a href="/wiki/Datei:BSicon_uHST.svg" class="image" title="&nbsp;&nbsp;&nbsp;"><img alt="&nbsp;&nbsp;&nbsp;" src="//upload.wikimedia.org/wikipedia/commons/thumb/2/2d/BSicon_uHST.svg/20px-BSicon_uHST.svg.png" decoding="async" width="20" height="20" srcset="//upload.wikimedia.org/wikipedia/commons/thumb/2/2d/BSicon_uHST.svg/30px-BSicon_uHST.svg.png 1.5x, //upload.wikimedia.org/wikipedia/commons/thumb/2/2d/BSicon_uHST.svg/40px-BSicon_uHST.svg.png 2x" data-file-width="500" data-file-height="500"></a></div>
+</td>
+<td style="text-align:right; vertical-align:middle; padding:0 .5em; font-size:80%; white-space:nowrap; border:none;">7,0
+</td>
+<td style="padding:0; border:none;" colspan="2"><a href="/wiki/U-Bahnhof_G%C3%B6rlitzer_Bahnhof" title="U-Bahnhof Görlitzer Bahnhof">Görlitzer Bahnhof</a> (Gr) <small><a href="/wiki/U-Bahn-Linie_3_(Berlin)" title="U-Bahn-Linie 3 (Berlin)"><img alt="U3" src="//upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Berlin_U3.svg/22px-Berlin_U3.svg.png" decoding="async" width="22" height="13" srcset="//upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Berlin_U3.svg/34px-Berlin_U3.svg.png 1.5x, //upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Berlin_U3.svg/44px-Berlin_U3.svg.png 2x" data-file-width="436" data-file-height="258"></a></small>
+</td></tr>
+
+
+
+
+<tr>
+<td style="text-align:center; padding:0; border:none; white-space:nowrap;"><div style="position:relative; z-index:2; background-color:#F9F9F9;" class="bs-icon"><a href="/wiki/Datei:BSicon_uBHF.svg" class="image" title="&nbsp;&nbsp;&nbsp;"><img alt="&nbsp;&nbsp;&nbsp;" src="//upload.wikimedia.org/wikipedia/commons/thumb/c/c8/BSicon_uBHF.svg/20px-BSicon_uBHF.svg.png" decoding="async" width="20" height="20" srcset="//upload.wikimedia.org/wikipedia/commons/thumb/c/c8/BSicon_uBHF.svg/30px-BSicon_uBHF.svg.png 1.5x, //upload.wikimedia.org/wikipedia/commons/thumb/c/c8/BSicon_uBHF.svg/40px-BSicon_uBHF.svg.png 2x" data-file-width="500" data-file-height="500"></a></div>
+</td>
+<td style="text-align:right; vertical-align:middle; padding:0 .5em; font-size:80%; white-space:nowrap; border:none;">6,3
+</td>
+<td style="padding:0; border:none;" colspan="2"><a href="/wiki/U-Bahnhof_Kottbusser_Tor" title="U-Bahnhof Kottbusser Tor">Kottbusser Tor</a> (Kbo) <small><a href="/wiki/U-Bahn-Linie_3_(Berlin)" title="U-Bahn-Linie 3 (Berlin)"><img alt="U3" src="//upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Berlin_U3.svg/22px-Berlin_U3.svg.png" decoding="async" width="22" height="13" srcset="//upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Berlin_U3.svg/34px-Berlin_U3.svg.png 1.5x, //upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Berlin_U3.svg/44px-Berlin_U3.svg.png 2x" data-file-width="436" data-file-height="258"></a> <a href="/wiki/U-Bahn-Linie_8_(Berlin)" title="U-Bahn-Linie 8 (Berlin)"><img alt="U8" src="//upload.wikimedia.org/wikipedia/commons/thumb/2/24/Berlin_U8.svg/22px-Berlin_U8.svg.png" decoding="async" width="22" height="13" srcset="//upload.wikimedia.org/wikipedia/commons/thumb/2/24/Berlin_U8.svg/34px-Berlin_U8.svg.png 1.5x, //upload.wikimedia.org/wikipedia/commons/thumb/2/24/Berlin_U8.svg/44px-Berlin_U8.svg.png 2x" data-file-width="436" data-file-height="258"></a></small>
+</td></tr>
+
+
+
+
+<tr>
+<td style="text-align:center; padding:0; border:none; white-space:nowrap;"><div style="position:relative; z-index:2; background-color:#F9F9F9;" class="bs-icon"><a href="/wiki/Datei:BSicon_uHST.svg" class="image" title="&nbsp;&nbsp;&nbsp;"><img alt="&nbsp;&nbsp;&nbsp;" src="//upload.wikimedia.org/wikipedia/commons/thumb/2/2d/BSicon_uHST.svg/20px-BSicon_uHST.svg.png" decoding="async" width="20" height="20" srcset="//upload.wikimedia.org/wikipedia/commons/thumb/2/2d/BSicon_uHST.svg/30px-BSicon_uHST.svg.png 1.5x, //upload.wikimedia.org/wikipedia/commons/thumb/2/2d/BSicon_uHST.svg/40px-BSicon_uHST.svg.png 2x" data-file-width="500" data-file-height="500"></a></div>
+</td>
+<td style="text-align:right; vertical-align:middle; padding:0 .5em; font-size:80%; white-space:nowrap; border:none;">5,4
+</td>
+<td style="padding:0; border:none;" colspan="2"><a href="/wiki/U-Bahnhof_Prinzenstra%C3%9Fe" title="U-Bahnhof Prinzenstraße">Prinzenstraße</a> (Pr) <small><a href="/wiki/U-Bahn-Linie_3_(Berlin)" title="U-Bahn-Linie 3 (Berlin)"><img alt="U3" src="//upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Berlin_U3.svg/22px-Berlin_U3.svg.png" decoding="async" width="22" height="13" srcset="//upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Berlin_U3.svg/34px-Berlin_U3.svg.png 1.5x, //upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Berlin_U3.svg/44px-Berlin_U3.svg.png 2x" data-file-width="436" data-file-height="258"></a></small>
+</td></tr>
+
+
+
+
+<
+</tbody></table>
+</td>
+
+
+
+
+
