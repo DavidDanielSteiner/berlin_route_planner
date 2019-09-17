6a030f7e2d97c28a7e15bf3b2a211472093abeb0
David Steiner <david.daniel.steiner@gmail.com>
2019-09-17 20:44:46 +0200

    add berlin streets data

--- scraper_python/scraper_allStations.py
+++ scraper_python/scraper_allStations.py
@@ -5,7 +5,7 @@ Created on Thu May 23 15:31:52 2019
 @author: David
 """
 
-import mysql.connector
+#import mysql.connector
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
@@ -58,6 +58,6 @@ df_allStations=pd.DataFrame(stations_data ,columns=['station_name','latitude','l
 df_allStations = df_allStations.dropna()
 df_allStations.drop_duplicates(subset ="station_name", keep = 'first', inplace = True) 
 
-df_allStations.to_csv ('station_data.csv', index = None, header=True) 
+df_allStations.to_csv ('C:/Users/David/Desktop/station_data.csv', index = None, header=True) 
 
 
--- scraper_python/scraper_subwayLines.py
+++ scraper_python/scraper_subwayLines.py
@@ -48,9 +48,9 @@ for i in range(1, 10):
 df=pd.DataFrame(stations_data ,columns=['station_name','station_id','line_name'])  
 #df.drop_duplicates(subset ="station_name", keep = 'first', inplace = True)  
    
-df.to_csv ('subway_lines.csv', index = None, header=True) 
+df.to_csv ('C:/Users/David/Desktop/subway_lines.csv', index = None, header=True) 
 
 #check which stations are missing 
-df_new = pd.merge(df, df_allStations, on='station_name', how='inner')
+#df_new = pd.merge(df, df_allStations, on='station_name', how='inner')
 #df_new_2 = df[(~df.station_name.isin(df_new.station_name))&(~df.station_name.isin(df_new.station_name))]
-df_new.to_csv ('merged_data.csv', index = None, header=True) 
+#df_new.to_csv ('merged_data.csv', index = None, header=True) 
